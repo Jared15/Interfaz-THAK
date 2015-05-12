ce184ac56f32cf3ca12ffb603ae406f4cb2ac20b
sala_a <sala_a@10.5.2.55>
2015-05-12 17:57:18 -0500

    Cambios Diagrama de clase 1

--- RMI/src/Conexion/RMI.java
+++ RMI/src/Conexion/RMI.java
@@ -1,8 +1,23 @@
 package Conexion;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+
+import Connection.RemoteObserver;
 
 
 public interface RMI extends Remote{
-	public String getPath() throws RemoteException;
+	public String getPath(int carta) throws RemoteException;
+	void addObserver(RemoteObserver o) throws RemoteException;
+    void notificarActualizacion(String color) throws RemoteException;
+    void crearJugador(String nombre, String pass,String PathAvatar) throws RemoteException;
+    void eliminarJugador(String nombre) throws RemoteException;
+    void actualizarJugador(String nombre, String pass,String PathAvatar) throws RemoteException;
+    void addJugador(String nombreJugador) throws RemoteException;
+    void getListaCartasMesa() throws RemoteException;
+    boolean verificarJugador(String nombre, String pass) throws RemoteException;
+    List<String> traerAvatar(String nombreUsuario) throws RemoteException;
+    List<List<String>> getListaJugadores() throws RemoteException;
+    void llenarMazo(int estilo) throws RemoteException;
+    void mostrarPrimeras3() throws RemoteException;
 }
